7d5e97936ad12a7b65224584d20a7e591de5fa1c
Alex <ateilhet@gmail.com>
2019-10-17 14:29:24 +0200

    wip: OAuth2 redirect

--- core/src/main/scala/org/thp/scalligraph/auth/OAuth2Srv.scala
+++ core/src/main/scala/org/thp/scalligraph/auth/OAuth2Srv.scala
@@ -1,13 +1,13 @@
 package org.thp.scalligraph.auth
 
 import javax.inject.{Inject, Singleton}
-import org.thp.scalligraph.OAuth2Redirect
+import org.thp.scalligraph.controllers.AuthenticatedRequest
 import play.api.libs.ws.WSClient
-import play.api.mvc.RequestHeader
+import play.api.mvc._
 import play.api.{Configuration, Logger}
 
-import scala.concurrent.Future
-import scala.util.{Failure, Try}
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 case class OAuth2Config(
     clientId: String,
@@ -23,27 +23,35 @@ case class OAuth2Config(
     autoUpdate: Boolean
 )
 
-class OAuth2Srv(OAuth2Config: OAuth2Config, userSrv: UserSrv, WSClient: WSClient) extends AuthSrv {
+class OAuth2Srv(OAuth2Config: OAuth2Config, userSrv: UserSrv, WSClient: WSClient)(implicit ec: ExecutionContext) extends AuthSrv {
   lazy val logger  = Logger(getClass)
   val name: String = "oauth2"
 
-  override def authenticate(username: String, password: String, organisation: Option[String])(implicit request: RequestHeader): Try[AuthContext] =
+  override def actionFunction(nextFunction: ActionFunction[Request, AuthenticatedRequest]): ActionFunction[Request, AuthenticatedRequest] =
     authRedirect
+      .andThen(super.actionFunction(nextFunction))
 
-  private def authRedirect: Try[AuthContext] = {
-    val queryStringParams = Map[String, Seq[String]](
-      "scope"         -> Seq(OAuth2Config.scope.mkString("+")),
+  private def authRedirect: ActionFilter[Request] = new ActionFilter[Request] {
+    private val queryStringParams = Map[String, Seq[String]](
+      "scope"         -> Seq(OAuth2Config.scope.mkString(" ")),
       "response_type" -> Seq(OAuth2Config.responseType),
       "redirect_uri"  -> Seq(OAuth2Config.redirectUri),
       "client_id"     -> Seq(OAuth2Config.clientId)
     )
+    def executionContext: ExecutionContext = ec
 
-    Failure(OAuth2Redirect(OAuth2Config.authorizationUrl, queryStringParams))
+    def filter[A](input: Request[A]): Future[Option[Result]] = Future.successful {
+      if (input.uri.contains("ssoLogin"))
+        Some(Results.Redirect(OAuth2Config.authorizationUrl, queryStringParams, status = 200))
+      else
+        None
+    }
   }
 }
 
 @Singleton
-class OAuth2Provider @Inject()(userSrv: UserSrv, config: Configuration, WSClient: WSClient) extends AuthSrvProvider {
+class OAuth2Provider @Inject()(userSrv: UserSrv, config: Configuration, WSClient: WSClient, implicit val executionContext: ExecutionContext)
+    extends AuthSrvProvider {
   override val name: String = "oauth2"
   override def apply(configuration: Configuration): Try[AuthSrv] =
     for {
